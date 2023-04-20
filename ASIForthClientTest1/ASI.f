79690101b8acddc58d032f3713b7c05d3bad4785
Andrew Read <andrew81244@outlook.com>
2023-04-20 12:21:23 +0800

    VFX Forth now successfully calls the ASI DLL (via the helper DLL)

--- ASIForthClientTest1/ASI.f
+++ ASIForthClientTest1/ASI.f
@@ -1,4 +1,14 @@
-LIBRARY: ASILibraryTest1.dll
-EXTERN: int "C" ConnectedCameraCount( void );
+CR
+LIBRARY: ASILibraryTest1.dll							\ place all DLL files in the VFX executable directory e.g. C:\MPE\VfxForth64\Bin
+EXTERN: int "C" ConnectedCameraCount( void ) ;	\ nota bene the space before the ;
 
-ConnectedCameraCount
+." ASILibraryTest1.dll load address "
+ASILibraryTest1.dll u. CR
+
+." ConnectedCameraCount load address "
+' ConnectedCameraCount func-loaded? u. CR
+
+.BadExterns CR
+
+." ConnectedCameraCount is "
+ConnectedCameraCount . CR
