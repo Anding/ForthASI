83ee2017e21c2df9104645cc9ef4182bc7ade3fb
Andrew <andrew81244@outlook.com>
2024-07-16 11:55:29 +0800

    developing ForthAstroCamera test

--- ForthASI/ForthAstroCamera_test1.f
+++ ForthASI/ForthAstroCamera_test1.f
@@ -4,6 +4,29 @@ include "%idir%\..\..\ForthBase\ForthBase.f"
 include "%idir%\ASI_SDK.f"
 include "%idir%\ASI_SDK_extend.f"
 include "%idir%\ForthAstroCamera.f"
+include "%idir%\ForthKMTronic\ForthKMTronic.f"
+
+-1 constant power-is-relay-switched
+
+power-is-relay-switched [IF] 
+\ Switch on the camera relay
+
+	6 constant COM-KMTronic
+	COM-KMTronic add-relays
+	1 relay-on
+	2000 ms
+	
+[THEN]
 
 CR
 scan-cameras
+
+power-is-relay-switched [IF]
+\ Switch off the camera relay
+
+	2000 ms
+	1 relay-off
+
+	remove-relays
+	
+[THEN]
